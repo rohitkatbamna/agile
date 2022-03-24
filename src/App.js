7b05b4dee182b4bce679e2bfcb04d100aa712aab
rohit katbamna <rohitkatbamna@protonmail.com>
2022-03-24 17:13:57 +0530

    24TH MARCH 2022 04:31 PM COMMIT NO. 2 VALUE NO.1 ADDED

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import React from "react";
+import "./scss/App.scss";
 
 function App() {
 	return (
 		<>
-			<div></div>
+			<div className="main_app_all_of_it">
+				<h1>The Agile Fundamentals</h1>
+				<h2>
+					Agile is a Philosophy guided by the Four Values and Twelve Principle.
+				</h2>
+				<h2>We Are Starting With Four Values</h2>
+				<h2>
+					Value No. 1 Individuals and Interactions Over Processes and Tools.
+				</h2>
+				<ul>
+					<li>
+						It's Important for People Building a software to interact with
+						Stackeholders(Customer).This not to say that processes and tools are
+						not important they are very important. But being Agile means placing
+						more emphasis on Interactions.
+					</li>
+					<li>
+						Interactions between team members or multiple team members are
+						important. Sometimes team members are hung up on how big this
+						software has to be, so what to do, the best way to solve this
+						problem is to have a face-to-face conversation with the team to
+						better develop and have a better estimation of work.
+					</li>
+				</ul>
+				<h2>
+					Key takaway here is Interactions are very important between
+					Stackeholders and teammembers.
+				</h2>
+			</div>
 		</>
 	);
 }

e6162f19f91cd7f92bcee89ac21c034e4c44425a
rohit katbamna <rohitkatbamna@protonmail.com>
2022-03-24 17:31:23 +0530

    24TH MARCH 2022 04:31 PM COMMIT NO. 3 VALUE NO.2 ADDED

--- src/App.js
+++ src/App.js
@@ -28,10 +28,39 @@ function App() {
 						better develop and have a better estimation of work.
 					</li>
 				</ul>
-				<h2>
+				<h4>
 					Key takaway here is Interactions are very important between
 					Stackeholders and teammembers.
-				</h2>
+				</h4>
+				<h2>Value No.2 Working Software Over Comprehensive Documentation.</h2>
+				<ul>
+					<li>
+						This does not mean we need to stop Documentation because without it
+						software navigation becomes very hard. The key word here is
+						Comprehensive.
+					</li>
+					<li>
+						What it means to say that focusing more on main deliverable software
+						rather then wasting a lot of time on documentation.
+					</li>
+					<li>
+						This can be achieved by{" "}
+						<span className="fw-bold">Shared Undestanding</span>. Having more
+						conversation between team and with the stakeholders and even the
+						customers.
+					</li>
+					<li>
+						This shared Undestanding can be achieve again by creating small
+						increaments of software and putting it in the hands of the internal
+						stakeholders or the customer so that they can gain feedback in the
+						software they want.
+					</li>
+				</ul>
+				<h4>
+					Key Takeaway here is creating small increatment in software and rasing
+					the shared understanding of customer and team without too much
+					focusing on documentation.
+				</h4>
 			</div>
 		</>
 	);

f9ba58c96368a82dabe92c60795984959a3ec954
rohit katbamna <rohitkatbamna@protonmail.com>
2022-03-31 12:34:43 +0530

    12:34 PM 12 VALUES COMPLETE

--- src/components/12pri/12pri.js
+++ src/components/12pri/12pri.js
@@ -102,6 +102,181 @@ function Twelve() {
 						building.
 					</li>
 				</ul>
+				<h2 className="fw-bold mt-3">Agile Principle No.5</h2>
+				<h2 className="fw-bold">
+					Build projects around motivated individuals. Give them the environment
+					and support they need, and trust them to get the job done.
+				</h2>
+				<ul>
+					<li>
+						The key concept here is: there's no project manager trying to ask
+						you, you know, what did you do yesterday? or like how much have you
+						done? Like no status reports. The idea here is, you know hire a
+						really good, motivated group of individuals, part of the team who
+						are cross-functional, and actually give them space to actually do
+						amazing work. And when we say work, not just devolve software but
+						also to solve problems for the company or the client.
+					</li>
+					<li>
+						So I think the big thing to keep in mind here if you're going to
+						work in an agile team is, regardless of your title these teams are
+						cross-functional. That means, they cross over roles. Your duties and
+						responsibilities are going to evolve and change and that is because,
+						everybody's collaborating together to try to realize that end value.
+					</li>
+				</ul>
+				<h2 className="fw-bold mt-3">Agile Principle No.6</h2>
+				<h2 className="fw-bold">
+					The most efficient and effective method of conveying information to
+					and within a development team is face to face conversation.
+				</h2>
+				<ul>
+					<li>
+						What this means is, instead of writing a big piece a requirement
+						document and giving to a developer, we are encouraging more face to
+						face communication. Or even worse creating a ticket, or messaging
+						them, and bothering them all the time, just having meetings face to
+						face, where you are able to communicate what you need.
+					</li>
+				</ul>
+				<h2 className="fw-bold mt-3">Agile Principle No.7</h2>
+				<h2 className="fw-bold">
+					Working software is the primary measure of progress.
+				</h2>
+				<ul>
+					<li>
+						This principle is just saying that, whenever you're thinking about
+						progress, let's measure that in terms of our working software. What
+						have we built? Not how much analysis we have done, or how many pages
+						of documentation we have created, or how many mock up pages we've
+						created? This is purely working software.
+					</li>
+					<li>
+						And while we may be creating models, as in part of that, and have
+						them attached to our various items. The working software is really
+						what we're driving towards and what will create the most value.
+					</li>
+				</ul>
+				<h2 className="fw-bold mt-3">Agile Principle No.8</h2>
+				<h2 className="fw-bold">
+					Agile processes promote sustainable development. The sponsors,
+					developers, and users should be able to maintain a constant pace
+					indefinitely.
+				</h2>
+				<ul>
+					<li>
+						In traditional days where business analyst is doing all kind of
+						analysis. They're stressed, and they hit their deadline, they
+						finish, they give it to a developer. Now a development team is
+						stressed out, and they're trying to make the ends meet, hit the
+						deadline, and then finally they're done. Then it goes to Tester.
+						It's Tester's time to be stressful now. They're testing everything,
+						and then implementation time comes in, and everybody stressed out.
+						So, in Agile, what we're trying to do is, instead of this big bang
+						thing wherein everybody's working in phases, we're talking about,
+						like, what are key features? What you need to do?. What is the
+						team's capacity? And think about a sustainable pace that team can
+						develop in.
+					</li>
+					<li>
+						I think its really important that because in Agile the whole team is
+						getting together, so, it makes it more sustainable, because it's a
+						cross-functional team, so, everybody is able to help move that that
+						particular project forward to meet the value.
+					</li>
+				</ul>
+				<h2 className="fw-bold mt-3">Agile Principle No.9</h2>
+				<h2 className="fw-bold">
+					Continuous attention to technical excellence and good design enhances
+					agility
+				</h2>
+				<ul>
+					<li>
+						This will tackle a little bit about quality in this principle. So,
+						if you build something and if there is no quality in place, you
+						cannot build anything on top of it. So, it's going to be harder for
+						you to be agile in the future. So, agile says: think about quality,
+						think about technical excellence, as you're building certain
+						products.
+					</li>
+					<li>
+						You evolve those features and functionality you've built, right?
+						You're making slight changes to them, and additions to them to help
+						drive value later on. If you've got problems early on in that
+						design, or in that initial quality, you're gonna have problems for a
+						long time.
+					</li>
+				</ul>
+				<h2 className="fw-bold mt-3">Agile Principle No.10</h2>
+				<h2 className="fw-bold">
+					Simplicity- the art of maximizing the amount of work not done- is
+					essential.
+				</h2>
+				<ul>
+					<li>
+						When I think about this principle Jeremy, I think about Google's
+						landing page. So when you go to Google's landing page, it is just
+						one search button and it's a white screen. And it is beautiful on
+						the way that it works. Just because you have more and more features,
+						it doesn't make a software pleasant to use. So the idea here, is in
+						Agile, you're also thinking about the outcome, and what ou-... what
+						kind of outcome are you looking. And you can do that by building
+						less and less, and delivering more.
+					</li>
+					<li>
+						So, making sure that you're delivering value on everything that
+						you're building, not just building something, or creating something
+						to create it.
+					</li>
+				</ul>
+				<h2 className="fw-bold mt-3">Agile Principle No.11</h2>
+				<h2 className="fw-bold">
+					The best architectures, requirements, and designs emerge from
+					self-organizing teams.
+				</h2>
+				<ul>
+					<li>
+						So in agile teams, where the teams are self-organizing and
+						cross-functional, meaning depending on what kind of project you're
+						working on, you're going to have people with the right skill sets.
+						And that's what a team is formed of. Is everybody in the team has
+						unique skill sets and they have what it takes to make this project
+						successful. So, when they need something- a question about an
+						architecture- they already have somebody in that team, so, they can
+						actually talk to somebody, versus borrowing somebody, from somebody
+						else.
+					</li>
+					<li>
+						In waterfall, or in other methodologies, a lot of times you need to
+						go ask a manager. "Hey, can I have somebody from your team- from
+						your testing team- to help out with this?". And it usually takes a
+						long time. Where[as] in agile, with it being self-organizing, if you
+						need somebody, you can go tap them on the shoulder and see if they
+						have availability to come help your team, or to join the team
+						potentially.
+					</li>
+				</ul>
+				<h2 className="fw-bold mt-3">Agile Principle No.12</h2>
+				<h2 className="fw-bold">
+					At regular intervals, the team reflects on how to become more
+					effective, then tunes and adjusts its behavior accordingly.
+				</h2>
+				<ul>
+					<li>
+						This is actually one of the key principle of agile, that one my
+						favorite principle of agile (instructor)as well. So, as you're
+						working in a product as a team, you're going to learn different
+						things about each other ways of working. So at each interval like
+						every two three weeks, you're getting back and you're looking at
+						what went well, what didn't go well, and how can we improve. and
+						this is a key principle of agile.
+					</li>
+					<li>
+						Agile, with it being more iterative, every few weeks, you're
+						actually able to look and see how it's going and make adjustments as
+						necessary, to make the team even more successful.
+					</li>
+				</ul>
 			</div>
 		</>
 	);

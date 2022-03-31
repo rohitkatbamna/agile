2f47878ccda3624f329b875e097b0cdd4eeebcfd
rohit katbamna <rohitkatbamna@protonmail.com>
2022-03-31 11:52:21 +0530

    11:51 AM WEBSITE AND 4 PRICIPLE COMPLETE

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Four Agile Values Completed
+
+# website https://grand-pegasus-0e6f0f.netlify.app/
--- src/components/12pri/12pri.js
+++ src/components/12pri/12pri.js
@@ -31,6 +31,77 @@ function Twelve() {
 					Welcome changing requirements, even late in development. Agile
 					processes harness change for the customer's competitive advantage.
 				</h2>
+				<ul>
+					<li>
+						Responding to change in 21st century is crucial, for any business,
+						even a small business, you need to be able to respond to change.
+					</li>
+					<li>
+						So, what this principle is saying is, if somebody is part of an
+						agile team and if they're delivering something to the customer,think
+						about being able to create something for the customer, in case
+						something changes on their business end.
+					</li>
+					<li>
+						It's also about delighting customers as well, and really responding
+						to their needs.
+					</li>
+				</ul>
+				<h2 className="fw-bold mt-3">Agile Principle No.3</h2>
+				<h2 className="fw-bold">
+					Deliver working software frequently, from a couple weeks to a couple
+					of months,with a preference to the shorter timescale.
+				</h2>
+				<ul>
+					<li>
+						So, in this principle what we are saying is: focus on the working
+						software. And when we say working software- it is not a requirement;
+						it is not a mockup; it is not a wireframe.{" "}
+						<span className="fw-bold"> IT IS [A] WORKING SOFTWARE</span> -
+						something that actually works, a part of the feature that you can
+						give it to the customer, receive feedback, and see how they use it.
+					</li>
+					<li>
+						And then the second thing is, the timescale.You are trying to shoot
+						for the shorter timescale a few weeks. And every few weeks, you're
+						trying to have some sort of working software, and you're trying to
+						give that to your customer.
+					</li>
+					<li>
+						So, the working software as you can give it to them (customer); so
+						they can get their hands on it. Because most change and most ideas
+						will come out when they actually start working with it and they can
+						see themselves trying to meet that business need. So, that... that's
+						absolutely crucial.
+					</li>
+				</ul>
+				<h2 className="fw-bold mt-3">Agile Principle No.4</h2>
+				<h2 className="fw-bold">
+					Business people and developers must work together daily, throughout
+					the project.
+				</h2>
+				<ul>
+					<li>
+						So here what Agile is saying is: There's no need for a creation of
+						this layer. Developers, the team, and the stakeholders or the
+						customers, can actually talk and in fact, it's encouraged, they're
+						talking on almost on a daily basis.
+					</li>
+					<li>
+						And the other thing to remember is: we're not talking about business
+						people asking developer when this is going to be done, when this is
+						going to be done? We're talking about a collaboration. What are we
+						building? Has anything changed? So, that kind of communication is
+						happening throughout.
+					</li>
+					<li>
+						With agile, it removes the blinders from the developer and allows
+						them to get their questions answered directly from the source rather
+						than going through a layer which doesn't really add much value. Yep,
+						it just helps everybody to get on the same page on what they're
+						building.
+					</li>
+				</ul>
 			</div>
 		</>
 	);
